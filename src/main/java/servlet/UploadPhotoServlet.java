dd1bbe27fe8914ca9c09cf5e7bf36fc764a81e76
Misha <misha_trunov@hotmail.com>
2017-05-31 00:43:14 +0300

    servlet for photo

---
+++ src/main/java/servlet/UploadPhotoServlet.java
@@ -0,0 +1,95 @@
+package servlet;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import service.UploadPhoto;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by misha on 30.05.17.
+ */
+
+@WebServlet(name="uploadPhoto", urlPatterns="/uploadPhoto")
+public class UploadPhotoServlet extends HttpServlet {
+
+    UploadPhoto uploadPhoto = new UploadPhoto();
+
+    private static final String UPLOAD_DIRECTORY = "photo";
+
+    private static final int MEMORY_THRESHOLD = 1024 * 1024 * 3; 	// 3MB
+    private static final int MAX_FILE_SIZE = 1024 * 1024 * 40;      // 40MB
+    private static final int MAX_REQUEST_SIZE = 1024 * 1024 * 50;   // 50MB
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.setCharacterEncoding("UTF-8");
+
+        // configures upload settings
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        factory.setSizeThreshold(MEMORY_THRESHOLD);
+        factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+
+        ServletFileUpload upload = new ServletFileUpload(factory);
+
+        upload.setFileSizeMax(MAX_FILE_SIZE);
+        upload.setSizeMax(MAX_REQUEST_SIZE);
+
+        String uploadPath = getServletContext().getRealPath("")
+                + File.separator + UPLOAD_DIRECTORY;
+
+        // creates the directory if it does not exist
+        File uploadDir = new File(uploadPath);
+        if (!uploadDir.exists()) {
+            uploadDir.mkdir();
+        }
+
+        try {
+            @SuppressWarnings("unchecked")
+            List<FileItem> formItems = upload.parseRequest(request);
+
+            if (formItems != null && formItems.size() > 0) {
+                // iterates over form's fields
+                for (FileItem item : formItems) {
+                    // processes only fields that are not form fields
+                    if (!item.isFormField()) {
+                        String fileName = new File(item.getName()).getName();
+                        String filePath = uploadPath + File.separator + fileName;
+                        File storeFile = new File(filePath);
+
+                        // saves the file on disk
+                        item.write(storeFile);
+                        request.setAttribute("message",
+                                "Photo has been uploaded successfully!");
+
+                        uploadPhoto.savePhoto(storeFile);
+
+                    }
+                }
+            }
+        }catch (Exception ex) {
+            request.setAttribute("message",
+                    "There was an error!" + "<br/>" +
+                            "Try again, and select image format please!");
+        }
+
+        getServletContext().getRequestDispatcher("/editProfile.jsp").forward(
+                request, response);
+    }
+}
